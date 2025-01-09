f5dc4207f95a9fc0e3c78745096832c08a5a41d4
Zapz <361250387@qq.com>
2025-01-09 10:59:09 +0800

    调整端口配置

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+// 启动时的服务配置，包括IP、端口、启动后是否自动打开浏览
+const _server_host = "0.0.0.0";
+const _server_port = "5174";
+const _server_open_browser = true;
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -12,9 +17,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true,
       },
     },
+    host: _server_host,
+    port: _server_port,
+    open: _server_open_browser,
   },
 }) 
